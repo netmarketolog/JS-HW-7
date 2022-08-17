b06222fc68cdaa06f7bf76a46e422d682099e873
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-08-17 22:06:10 +0300

    Add second hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,63 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('.js-gallery')
+
+const cardsMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+    })
+    .join('');
+
+
+galleryList.insertAdjacentHTML('afterbegin', cardsMarkup)
+
+lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+
+})
+
+
+// galleryList.addEventListener('click', onGalleryListClick)
+
+// let instance;
+
+// function onGalleryListClick(e) {
+//     e.preventDefault()
+
+//     if (!e.target.classList.contains('gallery__image')) {
+//         return
+//     }
+
+//     const url = getOriginalImgUrl(e);
+//     instance = createModal(url);
+
+//     instance.show(window.addEventListener('keydown', onEscClick))
+
+// };
+
+// function getOriginalImgUrl(e) {
+//     return e.target.dataset.source;
+// };
+
+// function createModal(url) {
+//     return basicLightbox.create(
+//         `<img src="${url}">`, {
+//             onClose: () => {
+//                 window.removeEventListener('keydown', onEscClick);
+//             }
+//         }
+//     );
+// };
+
+
+// function onEscClick(e) {
+//     if (e.code === 'Escape') {
+//         instance.close();
+//     }
+// };
